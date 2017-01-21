560c49cfdc35dece7c9a3cb0ceac938a9c64ba55
belfa <miguel36410@gmail.com>
2017-01-21 12:11:41 +0100

    Añadida conexión a MongoDB
    Hemos añadido Mongoose para tener una capa de abstracción sobre la base
    de datos en MongoDB.

--- api-restful-favoritos/index.js
+++ api-restful-favoritos/index.js
@@ -1,9 +1,17 @@
 'use strict'
 
+var mongoose = require('mongoose');
 var app = require('./app')
 var port  = process.env.PORT ||  3678;
 
-
-app.listen(port,() => {
-  console.log("API REST FAVORITOS funcionando en http://localhost:"+port);
+mongoose.connect('mongodb://localhost:27017/cursofavoritos',(err,res) => {
+  if(err){
+    console.log('Error en la conexión a MongodDb');
+    throw err;
+  }else{
+    console.log('Conexión a MongoDB correcta.')
+    app.listen(port,() => {
+      console.log("API REST FAVORITOS funcionando en http://localhost:"+port);
+    });
+  }
 });
