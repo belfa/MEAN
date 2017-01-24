7b2253b7ee348c7629cc0696bac8e16fdce79f5e
belfa <miguel36410@gmail.com>
2017-01-24 20:39:40 +0100

    Configurando CORS
    Configuramos las CORS (Control de acceso HTTP) que incluyen orígenes,
    cabeceras y métodos.

--- api-restful-favoritos/app.js
+++ api-restful-favoritos/app.js
@@ -7,6 +7,16 @@ var api   = require('./routes/favorito');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin','*');
+    response.header('Access-Control-Allow-Headers','X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
+    response.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, DELETE');
+    response.header('Allow','GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
+
 app.use('/api',api);
 
 
