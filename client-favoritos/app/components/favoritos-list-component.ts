3cc17b9b533c181f6cc1a57ff1d4e98df7eef8c8
belfa <miguel36410@gmail.com>
2017-02-07 22:55:20 +0100

    Directivas
    Nuestras primeras directivas.

--- client-favoritos/app/components/favoritos-list-component.ts
+++ client-favoritos/app/components/favoritos-list-component.ts
@@ -7,8 +7,26 @@ import {Component} from '@angular/core';
 
 export class FavoritosListComponent {
     public title : string;
+    public favoritos: Array<string>;
+    public favoritosVisibles: boolean;
+    public color: string;
 
     constructor(){
         this.title = 'Listado de marcadores: ';
+        this.favoritos = ['wwww.google.es','youtube.com','twitter.com'];
+        this.favoritosVisibles = true;
+        this.color = 'red';
+    }
+
+    public showFavoritos(){
+        this.favoritosVisibles = true;
+    }
+
+    public hideFavoritos(){
+        this.favoritosVisibles = false;
+    }
+
+    public changeColor(){
+        console.log(this.color);
     }
 }
