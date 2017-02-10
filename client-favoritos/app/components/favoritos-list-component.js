375ea2e3be86b57d9cf7a91f516c340ac6af3ad2
belfa <miguel36410@gmail.com>
2017-02-10 22:36:00 +0100

    Algo de maquetado
    Eliminamos la pruebas y comenzamos a esbozar nuestra aplicación. Para
    ello comenzamos con nociones básicas de maquinación.

--- client-favoritos/app/components/favoritos-list-component.js
+++ client-favoritos/app/components/favoritos-list-component.js
@@ -12,19 +12,7 @@ var core_1 = require("@angular/core");
 var FavoritosListComponent = (function () {
     function FavoritosListComponent() {
         this.title = 'Listado de marcadores: ';
-        this.favoritos = ['wwww.google.es', 'youtube.com', 'twitter.com'];
-        this.favoritosVisibles = true;
-        this.color = 'red';
     }
-    FavoritosListComponent.prototype.showFavoritos = function () {
-        this.favoritosVisibles = true;
-    };
-    FavoritosListComponent.prototype.hideFavoritos = function () {
-        this.favoritosVisibles = false;
-    };
-    FavoritosListComponent.prototype.changeColor = function () {
-        console.log(this.color);
-    };
     return FavoritosListComponent;
 }());
 FavoritosListComponent = __decorate([
